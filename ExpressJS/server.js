0aedc19a46b233ea44b433948e8dd42243bed07a
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-03 15:29:26 +0530

    refactor tea data structure to use an array and add GET endpoint for tea data

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -6,12 +6,11 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let teaData = {};
+let teaData = [];
 let nextId = 1;
 
 app.post("/tea", (req, res) => {
   const { name, price } = req.body;
-
   const newTea = {
     id: nextId++,
     name,
@@ -22,6 +21,9 @@ app.post("/tea", (req, res) => {
   res.status(200).send(newTea);
 });
 
+app.get("/tea", (req, res) => {
+    res.status(200).send(teaData);
+});
 app.listen(PORT, hostname, () => {
   console.log(`Server is running on  http://${hostname}:${PORT}`);
 });
