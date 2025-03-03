59c8e650e17653eced59ae08ca8e5d750b8a85eb
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-03 15:40:43 +0530

    add PUT and DELETE endpoints for tea with error handling

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -35,6 +35,32 @@ app.get("/tea/:id", (req, res) => {
     }
   });
 
+app.put("/tea/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, price } = req.body;
+    const tea = teaData.find((t) => t.id === id);
+    if (!tea) {
+      res.status(404).send("Tea not found");
+    } else {
+      tea.name = name;
+      tea.price = price;
+      res.status(200).send(tea);
+    }
+  }
+);
+
+app.delete("/tea/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const tea = teaData.find((t) => t.id === id);
+
+    if (!tea || teaData.length === 0 || id === -1) {
+      res.status(404).send("Tea not found");
+    } else {
+      teaData = teaData.filter((t) => t.id !== id);
+      res.status(200).send(tea);
+    }
+});
+
 app.listen(PORT, hostname, () => {
   console.log(`Server is running on  http://${hostname}:${PORT}`);
 });
