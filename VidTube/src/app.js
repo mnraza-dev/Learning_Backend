559c5a4e33d5251f547bfa7f78cbd7b868559a10
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-03 22:30:28 +0530

    db connection working

--- VidTube/src/app.js
+++ VidTube/src/app.js
@@ -1,9 +1,5 @@
-import dotenv from "dotenv";
 import express from 'express';
 import cors from "cors";
-
-
-dotenv.config({});
 const app = express();
 
 
--- VidTube/src/db/index.js
+++ VidTube/src/db/index.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import { DB_NAME } from '../constants.js';
-
-dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
     try {
@@ -10,17 +7,10 @@ const connectDB = async () => {
             console.error("MONGO DB CONNECTION ❌ Error: MONGODB_URI is not defined.");
             process.exit(1);
         }
-
-        console.log(`Connecting to MongoDB at: ${process.env.MONGODB_URI}/${DB_NAME}`);
-
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        console.log('\n✅ MONGODB CONNECTED 🚀');
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+         console.log('\n ✅ MONGODB CONNECTED 🚀');
     } catch (error) {
-        console.error(`MONGO DB CONNECTION ❌ Error: ${error.message}`);
+        console.error(`MONGODB CONNECTION ❌ Error: ${error.message}`);
         process.exit(1);
     }
 };
