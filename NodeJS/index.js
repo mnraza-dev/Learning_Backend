9b3367dddbea7b385deb4544424707bfa30ab47f
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-04 01:12:10 +0530

    feat: initialize Node.js application with basic server setup and dotenv configuration

---
+++ NodeJS/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import http from "http";
+
+dotenv.config();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World\n");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
