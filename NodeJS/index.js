23c923e99b3ad8cf2d6af9bee7ea57f2998c0ea1
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-08 20:49:13 +0530

    implement routing for home and about pages with 404 handling

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -4,11 +4,21 @@ import http from "http";
 dotenv.config();
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World\n");
+  if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ status: "ok", message: "About Route" }));
+  }
+  else if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to Home Route \n");
+  }
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
