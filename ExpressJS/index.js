9ac63d92ff5c4acf0d001e34643b2cba0b31bd70
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-08 20:55:41 +0530

    implement home and about routes with 404 handling and update start script

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -2,6 +2,19 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Home Route");
+});
+app.get("/about", (req, res) => {
+  res.status(200).send("Welcome to About Route");
+});
+
+
+
+app.use((req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port http://127.0.0.1:${PORT}`);
 });
