835cf26ed75cf7434be1526a3e8385a93842e803
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-10 16:22:58 +0530

    refactor user controller methods for improved readability and error handling

--- Learn_mongoDB/controllers/user.controllers.js
+++ Learn_mongoDB/controllers/user.controllers.js
@@ -16,14 +16,14 @@ export const createUser = async (req, res) => {
       .json({ message: "Error creating user", error: error.message });
     console.log(error);
   }
-}
+};
 export const fetchUser = async (req, res) => {
   const users = await User.find();
   res.status(200).json({
     message: "Users fetched successfully",
     data: users,
   });
-}
+};
 export const fetchUserByUser = async (req, res) => {
   try {
     const user = await User.findOne({
@@ -42,35 +42,34 @@ export const fetchUserByUser = async (req, res) => {
       data: null,
     });
   }
-}
+};
 export const updateUser = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { name, email } = req.body;
-      const user = await User.findOneAndUpdate(
-        { id },
-        {
-          name,
-          email,
-        },
-        { new: true }
-      );
-      if (!user) return res.status(404).json({ message: "User not found" });
-      res.status(200).json({ message: "User updated successfully", data: user });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Error updating user", error: error.message });
-    }
-}
-export const deleteUser = async (req, res) => {
-    try {
-      const user = await User.findOneAndDelete(req.params.id);
-      if (!user) return res.status(404).json({ message: "User not found" });
-      res.status(200).json({ message: "User deleted successfully", data: null });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Error deleting user", error: error.message });
-    }
+  try {
+    const { name, email } = req.body;
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name,
+        email,
+      },
+      { new: true }
+    );
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User updated successfully", data: user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating user", error: error.message });
   }
+};
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully", data: null });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting user", error: error.message });
+  }
+};
