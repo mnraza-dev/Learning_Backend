447d87c8b961bed60f248325014d86e74c77af68
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-14 04:15:19 +0530

    add user routes and controller; implement CRUD operations for users

---
+++ youtube-backend-app/src/controllers/user.controllers.js
@@ -0,0 +1,15 @@
+export const getAllUsers = (req, res) => {
+  res.send("Get all users");
+};
+export const getUserById = (req, res) => {
+  res.send("Get user");
+};
+export const createUser = (req, res) => {
+  res.send("Create user");
+};
+export const updateUser = (req, res) => {
+  res.send("Update user");
+};
+export const deleteUser = (req, res) => {
+  res.send("Delete user");
+};
--- youtube-backend-app/src/index.js
+++ youtube-backend-app/src/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.config.js";
+import userRouter from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
