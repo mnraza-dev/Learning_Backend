61feed9f4d2f2e005ef56f9c246640da4ac6c550
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-14 03:26:28 +0530

    add dotenv and express dependencies; implement basic Hello World route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1> Hello World!</h1>");
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
