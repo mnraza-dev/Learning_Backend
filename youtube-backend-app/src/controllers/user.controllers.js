e9ba63a2a38f6d72eb5da243a4334478af69ccd7
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-14 20:57:11 +0530

    add user login functionality with JWT authentication

--- youtube-backend-app/src/controllers/user.controllers.js
+++ youtube-backend-app/src/controllers/user.controllers.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { User } from "../models/user.models.js";
 import cloudinary from "../config/cloudinary.config.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req, res) => {
   try {
@@ -31,6 +32,51 @@ export const createUser = async (req, res) => {
     res.status(201).json({
       message: "User created successfully",
       user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+export const loggedInUser = async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        channelName: existingUser.channelName,
+        email: existingUser.email,
+        phone: existingUser.phone,
+        logoId: existingUser.logoId,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10d",
+      }
+    );
+    res.status(200).json({
+      _id: existingUser._id,
+      channelName: existingUser.channelName,
+      email: existingUser.email,
+      phone: existingUser.phone,
+      logoId: existingUser.logoId,
+      logoUrl: existingUser.logoUrl,
+      token: token,
+      subscribers: existingUser.subscribers,
+      subscribedChannels: existingUser.subscribedChannels,
+      message: "User logged in successfully",
     });
   } catch (error) {
     console.error(error);
--- youtube-backend-app/src/routes/user.routes.js
+++ youtube-backend-app/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser, getAllUsers, getUserById } from "../controllers/user.controllers.js";
+import { createUser, getAllUsers, getUserById, loggedInUser } from "../controllers/user.controllers.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", createUser);
+userRouter.post("/login", loggedInUser);
 userRouter.get("/", getAllUsers);
 userRouter.get("/:id", getUserById);
 
