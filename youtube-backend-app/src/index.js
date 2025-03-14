94364a5c389275e6fa4327605338a070208a55a2
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-14 16:13:45 +0530

    update user model to include additional fields and change API route structure

--- youtube-backend-app/src/index.js
+++ youtube-backend-app/src/index.js
@@ -9,12 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
