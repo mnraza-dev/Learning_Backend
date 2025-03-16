9b77e463cbf6d98c7b4fa4f04051a6c03ae9aa84
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-16 21:37:25 +0530

    add Cloudinary integration for image uploads and update package dependencies

--- authentication/backend/config/cloudinary.config.js
+++ authentication/backend/config/cloudinary.config.js
@@ -1,12 +1,10 @@
 import { V2 as cloudinary } from "cloudinary";
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
-
 const uploadImage = async (filePath) => {
   try {
     const result = await cloudinary.uploader.upload(filePath, {
@@ -14,9 +12,7 @@ const uploadImage = async (filePath) => {
     });
     return result.secure_url;
   } catch (error) {
- 
     console.log(error);
   }
 };
-
 export default cloudinary;
