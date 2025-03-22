323c63cf3376fc9e4344533360997c9e6baff755
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-22 22:05:15 +0530

    add additional os module examples for system information

--- NodeJS/Node_Module_System/2_os.js
+++ NodeJS/Node_Module_System/2_os.js
@@ -3,4 +3,14 @@ const os = require("os");
 console.log(os.arch()); // x64
 console.log(os.platform()); // win32
 
+console.log(os.homedir());
+console.log(os.hostname()); 
+console.log(os.release());
+
+console.log('NETWORK: ', os.networkInterfaces());
+
+console.log(os.cpus().length);
+console.log(os.cpus());
+console.log(os.totalmem());
+console.log(os.uptime());
 
