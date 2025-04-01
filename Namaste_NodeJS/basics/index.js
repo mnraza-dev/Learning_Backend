1accee8d903341d4d42151f35f5c3431be096918
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-01 05:36:03 +0530

    fix: add missing newline at end of fs.js; create index.js for global context examples

---
+++ Namaste_NodeJS/basics/index.js
@@ -0,0 +1,10 @@
+console.log(global);
+
+console.log(this); // empty object
+console.log(globalThis === global);
+// globalThis is the same as global and it is available in all browsers and servers
+
+
+
+
+
