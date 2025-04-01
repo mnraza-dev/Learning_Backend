3e5f0dafe91b9d7f7cd1fe04e52b69f5cb96c0bf
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-01 05:52:10 +0530

    add: create sum.js and xyz.js for sum calculation; update index.js to require xyz.js

--- Namaste_NodeJS/basics/index.js
+++ Namaste_NodeJS/basics/index.js
@@ -1,7 +1,8 @@
-console.log(global);
-
+require('./xyz');	
+// console.log(global);
 console.log(this); // empty object
 console.log(globalThis === global);
+
 // globalThis is the same as global and it is available in all browsers and servers
 
 
---
+++ Namaste_NodeJS/basics/sum.js
@@ -0,0 +1,5 @@
+function calculateSum(a, b) {
+  const sum = a + b;
+  return sum;
+}
+module.exports = calculateSum;
