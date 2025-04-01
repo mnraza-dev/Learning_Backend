0311257b2c5a02ce8fb122a0b33a8085afbcfe6d
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-02 03:29:22 +0530

    fix: update app.js to send HTML response and correct main entry point in package.json; fix typo in express dependency

--- Namaste_NodeJS/DevTinder/backend/src/app.js
+++ Namaste_NodeJS/DevTinder/backend/src/app.js
@@ -6,8 +6,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Hello World</h1>");
 });
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
