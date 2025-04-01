9e690887d0af3f543ede24ec1c3c9992ceb1adf5
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-02 03:25:32 +0530

    feat: set up Express server with basic routing and environment configuration; update package.json for new dependencies

--- Namaste_NodeJS/DevTinder/backend/src/app.js
+++ Namaste_NodeJS/DevTinder/backend/src/app.js
@@ -1,1 +1,13 @@
-console.log('hello world')	;
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
