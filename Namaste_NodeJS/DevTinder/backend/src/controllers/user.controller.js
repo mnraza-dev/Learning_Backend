1e7c22b5b6a599429a928a5260e490b3133fca25
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-02 06:24:40 +0530

    feat: add user feed and get user by email endpoints in user controller and routes

--- Namaste_NodeJS/DevTinder/backend/src/controllers/user.controller.js
+++ Namaste_NodeJS/DevTinder/backend/src/controllers/user.controller.js
@@ -28,3 +28,18 @@ export const login = async (req, res) => {
 export const test = async (req, res) => {
   res.json({ message: "Hello World" });
 };
+
+export const feed = async (req, res) => {
+  const users = await User.find();
+  if (!users) {
+    return res.status(404).json({ message: "Users not found" });
+  }
+  res.json(users);
+};
+export const getUserByEmail = async (req, res) => {
+  const user = await User.findOne({ email: req.params.email });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.json(user);
+};
--- Namaste_NodeJS/DevTinder/backend/src/routes/user.routes.js
+++ Namaste_NodeJS/DevTinder/backend/src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { login, signup, test } from "../controllers/user.controller.js";
+import {
+  feed,
+  getUserByEmail,
+  login,
+  signup,
+  test,
+} from "../controllers/user.controller.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", test);
+userRoutes.get("/:email", getUserByEmail);
+userRoutes.get("/feed", feed);
 userRoutes.post("/signup", signup);
 userRoutes.post("/login", login);
 
