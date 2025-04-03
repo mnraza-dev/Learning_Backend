4cdd892d1c8f7555bfb162328021636c130f3b44
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-03 10:49:16 +0530

    feat: implement basic layout with Login, Body, and Profile pages; add Navbar and Footer components; integrate Tailwind CSS and DaisyUI

--- Namaste_NodeJS/DevTinder/frontend/src/App.jsx
+++ Namaste_NodeJS/DevTinder/frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+             </Routes>
+        </BrowserRouter>
+      </div>
 
-      <h1>Hello</h1>
+      <Footer />
     </>
   );
 };
