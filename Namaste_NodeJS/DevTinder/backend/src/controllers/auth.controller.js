df388c9662aeab9107264e53d61d0501c81aadd3
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-03 07:13:58 +0530

    feat: remove unused user-related functions and test route from auth controller and routes

--- Namaste_NodeJS/DevTinder/backend/src/controllers/auth.controller.js
+++ Namaste_NodeJS/DevTinder/backend/src/controllers/auth.controller.js
@@ -3,9 +3,6 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 import jwt from "jsonwebtoken";
 
-export const test = (req, res) => {
-  res.status(200).json({ message: "User route" });
-};
 export const signup = async (req, res) => {
   try {
     const password = req.body.password;
@@ -78,51 +75,6 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token");
     res.send("User logged out successfully");
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-export const getAllUsers = async (req, res) => {
-  const users = await User.find();
-  if (!users) {
-    return res.status(404).json({ message: "Users not found" });
-  }
-  res.json(users);
-};
-export const getUserByEmail = async (req, res) => {
-  const user = await User.findOne({ email: req.params.email });
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  res.json(user);
-};
-export const deleteUser = async (req, res) => {
-  const userName = req.params.userName;
-  try {
-    const user = await User.findOneAndDelete({
-      userName: userName,
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    res.json({ message: "User deleted successfully" });
-  } catch (error) {
-    console.log("error: ", error);
-    return res.status(500).json({ message: error.message });
-  }
-};
-export const updateUser = async (req, res) => {
-  try {
-    const userName = req.params.userName;
-    const user = await User.findOneAndUpdate(
-      { userName: userName },
-      { ...req.body },
-      { new: true }
-    );
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    res.json(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- Namaste_NodeJS/DevTinder/backend/src/routes/auth.routes.js
+++ Namaste_NodeJS/DevTinder/backend/src/routes/auth.routes.js
@@ -3,12 +3,10 @@ import {
   signup,
   login,
   logout,
-  test,
 } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.get("/test", test);
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
