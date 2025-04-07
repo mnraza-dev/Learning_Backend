5a538f2c23ba7764e71733e3676a63f13f7bb9eb
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-07 06:00:50 +0530

    feat: update index.js to return JSON responses; add user and course routes files

--- backend-cohort-3/Course-Selling-App/index.js
+++ backend-cohort-3/Course-Selling-App/index.js
@@ -6,9 +6,34 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
 app.get("/", (req, res) => {
-  res.send("Welcome to the Course Selling App!");
+  res.json({ message: "Welcome to the Course Selling App!" });
+});
+
+app.post("/user/signup", (req, res) => {
+  res.json({
+    message: "User Signed Up successfully!",
+  });
+});
+app.post("/user/login", (req, res) => {
+  res.json({
+    message: "User Logged In successfully!",
+  });
+});
+app.post("/user/purchases", (req, res) => {
+  res.json({
+    message: "Course added successfully!",
+  });
+});
+app.post("/course/purchase", (req, res) => {
+  res.json({
+    message: "Course added successfully!",
+  });
+});
+app.get("/courses", (req, res) => {
+  res.json({
+    message: "All courses fetched successfully!",
+  });
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
