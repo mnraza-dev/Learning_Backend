8cf2eb2e65fdadab1e5a0b62cabf534f8a85a057
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-07 14:53:30 +0530

    Assoignment 02 done ✅

--- backend-cohort-3/AuthWithJWT/index.js
+++ backend-cohort-3/AuthWithJWT/index.js
@@ -21,5 +21,28 @@ function signJwt(username, password) {
   return signature;
 }
 
+function verifyJwt(token) {
+  const verified = jwt.verify(token, jwtSecret);
+  if (verified) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function decodeJwt(token) {
+  const decoded = jwt.decode(token);
+  if (decoded) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 const ans = signJwt("mnraza@gmail.com", "mnr0za");
-console.log(ans);
+console.log("here is the token genrated: ", ans);
+
+const myToken = ans;
+console.log("Decoded is - ",decodeJwt(myToken));
+
+const verifiedT = verifyJwt(ans);
+console.log("Is this verified - ", verifiedT);
