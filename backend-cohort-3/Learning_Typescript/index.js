ee952f6dda9ac4b88d5b45392701fd818c4a36c3
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-08 14:46:52 +0530

    Typescript types like  : string and number

--- backend-cohort-3/Learning_Typescript/index.js
+++ backend-cohort-3/Learning_Typescript/index.js
@@ -1,4 +1,22 @@
 "use strict";
 let x = 8;
-x = "hjdkd";
 console.log(x);
+// Problem - 01 
+function greet(firstName) {
+    return "Hello " + firstName;
+}
+console.log(greet("Abdul"));
+// Problem - 02
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(4, 6));
+// Problem - 03
+function delayedCall(fn) {
+    setTimeout(fn, 2000);
+}
+delayedCall(function () {
+    setTimeout(() => {
+        console.log("Hi after 2 seconds");
+    }, 1000);
+});
