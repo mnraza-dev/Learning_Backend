83a0eaac2ec1dfe38128073d8b9fa6587089c2dd
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-17 10:08:40 +0530

    Update App component to include additional Recoil state values for jobs, messaging, and notifications; enhance network button display logic.

--- backend-cohort-3/Learn_Recoil/src/App.tsx
+++ backend-cohort-3/Learn_Recoil/src/App.tsx
@@ -1,14 +1,20 @@
 import { useRecoilValue } from "recoil"
-import { networkAtom } from "./atoms"
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from "./atoms"
 import { JSX } from "react"
 
 const App = (): JSX.Element => {
   const networkCount = useRecoilValue<number>(networkAtom)
+  const jobsCount = useRecoilValue<number>(jobsAtom)
+  const notificationCount = useRecoilValue<number>(notificationAtom)
+  const messageCount = useRecoilValue<number>(messagingAtom)
 
   return (
     <div>
       <h1>Learn Recoil</h1>
-      <button>My Network {networkCount}</button>
+      <button>My Network {networkCount > 99 ? "99+" : networkCount}</button>
+      <button>Jobs {jobsCount}</button>
+      <button>Messaging {messageCount}</button>
+      <button>Notifications {notificationCount}</button>
     </div>
   )
 }
