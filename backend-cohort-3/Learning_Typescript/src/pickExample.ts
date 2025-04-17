49000b135dcb2e8c748bbc3fd92863d2f7f900b3
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-17 17:11:13 +0530

    Add pickExample.ts file with NewUser type and updateUser function

---
+++ backend-cohort-3/Learning_Typescript/src/pickExample.ts
@@ -0,0 +1,13 @@
+// Pick allows you to create a new type by seelecting a set of properties (keys)
+//  from an existing type (Type) 
+type NewUser = {
+    id: number,
+    name: string,
+    age: number,
+    email: string,
+    password: string
+}
+type UpdateProps = Pick<NewUser, "name" | "age" | "email">
+function updateUser(updatedprops: UpdateProps) {
+    // hit the db
+}
