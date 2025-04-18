9d3eae1633f0ea99b0c22cd14486717841038faf
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-18 17:03:57 +0530

    feat: update dependencies and add Prisma support
    - Added @prisma/client and prisma to dependencies in package.json
    - Updated package-lock.json with new dependencies and versions
    - Modified index.ts to include type annotations for Express
    - Updated tsconfig.json to set rootDir and outDir for TypeScript compilation
    - Created .gitignore to exclude node_modules and environment variables
    - Added Prisma schema file for PostgreSQL datasource configuration

--- ecommerce/src/index.ts
+++ ecommerce/src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 
-const app = express();
+const app: Express = express();
 const port = 3000;
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 app.listen(port, () => {
