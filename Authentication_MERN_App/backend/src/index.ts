e1ddd35ea9548601d7690c16913bfa4c7fe4b52f
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-18 17:33:40 +0530

    feat: enhance server setup with middleware and environment mode logging

--- Authentication_MERN_App/backend/src/index.ts
+++ Authentication_MERN_App/backend/src/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 const port = process.env.PORT || 5000;
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
 app.listen(port, () => {
     connectDB();
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port} on ${process.env.NODE_ENV} mode`);
 })
