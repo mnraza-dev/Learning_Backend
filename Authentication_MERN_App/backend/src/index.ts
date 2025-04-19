93d61642e3205f6f7478f1ac2e7306b6e5add7a3
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-19 13:15:29 +0530

    refactor: update package.json scripts and clean up TypeScript configuration

--- Authentication_MERN_App/backend/src/index.ts
+++ Authentication_MERN_App/backend/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 app.use(cors(
     {
         origin: process.env.ORIGIN,
@@ -18,10 +18,9 @@ app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
 app.listen(port, () => {
     connectDB();
     console.log(`⚡️[server]: Server is running at http://localhost:${port} on ${process.env.NODE_ENV} mode`);
