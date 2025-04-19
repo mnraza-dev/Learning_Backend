67c1d4ad788f678ac047dee8495e66005884ce48
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-20 03:52:29 +0530

    feat: update frontend package name and add Tailwind CSS integration
    - Changed package name from "frontend" to "frontend-second-brain" in package.json
    - Added Tailwind CSS and its Vite plugin as dependencies
    - Updated pnpm-lock.yaml to include new dependencies
    - Created Button component with size and variant props
    - Implemented PlusIcon component for use with Button
    - Updated App component to demonstrate Button usage with icons
    - Configured Vite to use Tailwind CSS
    - Added index.css to import Tailwind CSS styles

--- backend-cohort-3/second-brain/frontend/src/App.tsx
+++ backend-cohort-3/second-brain/frontend/src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Button from './components/ui/Button'
+import PlusIcon from './icons/PlusIcon'
 
 const App: React.FC = () => {
   return (
-    <div>App</div>
+    <div className='flex flex-col gap-4 justify-center items-center'>
+
+      <Button size='sm' variant="primary" startIcon={<PlusIcon size='sm' />} onClick={() => {
+        console.log("Button 1 clicked")
+      }} text={"Button 1"} /> 
+      <Button size='md' variant="primary" startIcon={<PlusIcon size='md' />} onClick={() => {
+        console.log("Button 1 clicked")
+      }} text={"Button 1"} /> 
+      <Button size='lg' variant="primary" startIcon={<PlusIcon size='lg' />} onClick={() => {
+        console.log("Button 1 clicked")
+      }} text={"Button 1"} />
+
+    </div>
   )
 }
 
---
+++ backend-cohort-3/second-brain/frontend/src/components/ui/Button.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react'
+
+interface ButtonProps {
+    variant: 'primary' | 'secondary'
+    size: 'sm' | 'md' | 'lg'
+    text: string
+    startIcon?: ReactElement
+    endIcon?: ReactElement
+    onClick: () => void
+}
+const defaultStyles = "flex w-fit gap-2 items-center justify-center cursor-pointer rounded-lg"
+const variantStyles = {
+    "primary": "bg-blue-500 hover:bg-blue-700 text-white font-semibold ",
+
+    "secondary": "bg-[#dabbff] hover:bg-[#b792e3]  text-[#7e5da6] hover:text-[#3e265c] font-semibold",
+}
+const sizeStyles = {
+    "sm": "px-2 py-1 text-sm",
+    "md": "px-4 py-2 text-md",
+    "lg": "px-6 py-4 text-xl",
+}
+const Button = (props: ButtonProps) => {
+    return (
+        <button className={`${defaultStyles} ${variantStyles[props.variant]} ${sizeStyles[props.size]}`}
+            onClick={props.onClick} >
+        {props.startIcon}
+           {props.text}
+        </button>
+    )
+}
+export default Button
