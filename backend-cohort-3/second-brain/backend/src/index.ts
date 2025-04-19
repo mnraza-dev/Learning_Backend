d972716accbb836949052a796e5f203fb8af4b84
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-19 15:45:35 +0530

    fix: update package.json and pnpm-lock.yaml to include @types/jsonwebtoken dependency; refactor server.ts to improve code structure and add JWT functionality

--- backend-cohort-3/second-brain/backend/src/index.ts
+++ backend-cohort-3/second-brain/backend/src/index.ts
@@ -1,11 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/db";
 import app from "./server"
 dotenv.config();
 const port = process.env.PORT || 3000;
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
     connectDB();
     console.log(`Server is running at http://localhost:${port}`);
--- backend-cohort-3/second-brain/backend/src/server.ts
+++ backend-cohort-3/second-brain/backend/src/server.ts
@@ -1,8 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import User from "./models/User";
+import jwt from "jsonwebtoken";
+const app = express();
 
-const app: Application = express();
-app.post("/api/v1/signup", async (req: Request, res: Response) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/v1/signup", async (req, res) => {
     const { username, email, password } = req.body;
     try {
         const user = await User.create({ username, email, password });
@@ -18,22 +22,48 @@ app.post("/api/v1/signup", async (req: Request, res: Response) => {
             success: false
         });
     }
-})
-app.post("/api/v1/signin", (req, res) => {
-    console.log(req.body);
-    res.send("siggnin!");
-})
-app.post("/api/v1/content", (req, res) => {
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const token = jwt.sign(
+                { id: existingUser._id },
+                process.env.JWT_SECRET!,
+                { expiresIn: "1d" }
+            );
+
+            res.status(200).json({
+                message: "Signin successful",
+                token,
+                success: true
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            success: false
+        });
+    }
+});
+
+app.post("/api/v1/content", (req: Request, res: Response) => {
     console.log(req.body);
     res.send("content!");
-})
-app.post("/api/v1/brain/share", (req, res) => {
+});
+
+app.post("/api/v1/brain/share", (req: Request, res: Response) => {
     console.log(req.body);
     res.send("Signup!");
-})
-app.get("/api/v1/brain/:shareLink", (req, res) => {
+});
+
+app.get("/api/v1/brain/:shareLink", (req: Request, res: Response) => {
     console.log(req.body);
     res.send("Signup!");
-})
+});
 
 export default app;
