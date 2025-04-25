71906f5fe9f03670efbbc4163aac8f17f28328b2
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-25 19:25:46 +0530

    creating backned for paytm clone

--- backend-cohort-3/Learning_Monorepos/chat-app/apps/web/app/page.tsx
+++ backend-cohort-3/Learning_Monorepos/chat-app/apps/web/app/page.tsx
@@ -1,25 +1,15 @@
 import React from 'react'
 import { TextInput } from '@repo/ui/text-input'
 import { Button } from '@repo/ui/button'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const router = useRouter()
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-      <div style={{
-        display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center',
-
-      }}>
+      <div className=' flex flex-col gap-2'>
         <TextInput placeholder="Enter Email" />
-        <Button appName="web" className='' children="Join Room" />
+        <Button  appName="web" className='' children="Join Room" />
       </div>
-
-    </div>
   )
 }
-
 export default Home
--- backend-cohort-3/Learning_Monorepos/chat-app/packages/ui/src/button.tsx
+++ backend-cohort-3/Learning_Monorepos/chat-app/packages/ui/src/button.tsx
@@ -6,13 +6,16 @@ interface ButtonProps {
   children: ReactNode;
   className?: string;
   appName: string;
+
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => {
+       alert(`Button clicked for ${appName}`);
+      }}
     >
       {children}
     </button>
