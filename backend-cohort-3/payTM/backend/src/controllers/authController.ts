bd5517155f53996b72b2040a2e6f7a3fcabb6eb8
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-25 19:59:42 +0530

    feat: add authentication routes and controller for user signup and login

---
+++ backend-cohort-3/payTM/backend/src/controllers/authController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const createUser = (req: Request, res: Response,) => {
+    res.send("Create User")
+}
+export const loginUser = (req: Request, res: Response,) => {
+    res.send("Create User")
+}
---
+++ backend-cohort-3/payTM/backend/src/routes/authRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createUser, loginUser } from "../controllers/authController";
+
+const authRouter = Router();
+
+authRouter.post("/signup", createUser);
+authRouter.post("/login", loginUser);
+
