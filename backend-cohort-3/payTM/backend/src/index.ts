2b400db082c485b458926718afd5a74b82661222
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-25 19:35:10 +0530

    feat: add mongoose dependency and implement user model with routes

--- backend-cohort-3/payTM/backend/src/index.ts
+++ backend-cohort-3/payTM/backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+
+    console.log(`Server started at http://localhost:${port}`);
+});
