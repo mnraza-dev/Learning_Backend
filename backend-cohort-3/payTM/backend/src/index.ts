16fe36c2314a28078162804a733ffbc513b4d125
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-25 20:02:57 +0530

    feat: add @types/cors to devDependencies and update index.ts to use cors middleware

--- backend-cohort-3/payTM/backend/src/index.ts
+++ backend-cohort-3/payTM/backend/src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/userRoutes";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("api/v1",userRouter);
+app.use("api/v1", userRouter);
 app.listen(port, () => {
 
     console.log(`Server started at http://localhost:${port}`);
