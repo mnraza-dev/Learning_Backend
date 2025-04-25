59deeca34fb178376794f7d25439ee6bb928540c
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-25 19:36:38 +0530

    feat: integrate user routes into the main application

--- backend-cohort-3/payTM/backend/src/index.ts
+++ backend-cohort-3/payTM/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("api/v1",userRouter);
 app.listen(port, () => {
 
     console.log(`Server started at http://localhost:${port}`);
